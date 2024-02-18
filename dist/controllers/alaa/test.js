3e7f8b81bbad10f7ed02692049a39254654da937
Alaa Abukmeil <alaaabukmeil@Alaas-MacBook-Air.local>
2024-02-18 21:53:01 +0800

    test

---
+++ dist/controllers/alaa/test.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createDate = void 0;
+function createDate() {
+    let date = new Date();
+    return "Today is: " + date;
+}
+exports.createDate = createDate;
---
+++ dist/routes/router.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const test_1 = require("../controllers/alaa/test");
+require("dotenv").config();
+const multerGoogleStorage = require("multer-google-storage");
+const multer = require("multer");
+const router = (0, express_1.Router)();
+router.get("/", async (req, res, next) => {
+    res.send({ date: (0, test_1.createDate)() });
+});
+exports.default = router;
