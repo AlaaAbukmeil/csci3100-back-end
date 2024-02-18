f7387229b88522821b55e29661aa7ce21b524411
Alaa Abukmeil <alaaabukmeil@Alaas-MacBook-Air.local>
2024-02-18 21:55:37 +0800

    testing last 9:55 pm

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,8 +1,7 @@
 import { NextFunction, Router } from "express";
 
 import { Request, Response } from "express";
-
-
+import { createDate } from "../controllers/alaa/test";
 require("dotenv").config();
 
 const multerGoogleStorage = require("multer-google-storage");
@@ -11,7 +10,7 @@ const multer = require("multer");
 const router = Router();
 
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
-  res.sendStatus(200);
+  res.send(createDate());
 });
 
 export default router;
